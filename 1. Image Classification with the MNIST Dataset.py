7e584ddbff4e1d08e9409d94f10331eaab578c60
B P <braedanpurcell@gmail.com>
2024-05-30 23:13:49 -0400

    adding asl dataset solution

--- 1. Image Classification with the MNIST Dataset.py
+++ 1. Image Classification with the MNIST Dataset.py
@@ -9,6 +9,44 @@ import torchvision.transforms.v2 as transforms
 import torchvision.transforms.functional as F
 import matplotlib.pyplot as plt
 
+# Train Function
+def train():
+    loss = 0
+    accuracy = 0
+
+    model.train()
+    for x, y in train_loader:
+        x, y = x.to(device), y.to(device)
+        output = model(x)
+        optimizer.zero_grad()
+        batch_loss = loss_function(output, y)
+        batch_loss.backward()
+        optimizer.step()
+
+        loss += batch_loss.item()
+        accuracy += get_batch_accuracy(output, y, train_N)
+    print('Train - Loss: {:.4f} Accuracy: {:.4f}'.format(loss, accuracy))
+
+# Validation Function
+def validate():
+    loss = 0
+    accuracy = 0
+
+    model.eval()
+    with torch.no_grad():
+        for x, y in valid_loader:
+            x, y = x.to(device), y.to(device)
+            output = model(x)
+
+            loss += loss_function(output, y).item()
+            accuracy += get_batch_accuracy(output, y, valid_N)
+    print('Valid - Loss: {:.4f} Accuracy: {:.4f}'.format(loss, accuracy))
+
+def get_batch_accuracy(output, y, N):
+    pred = output.argmax(dim=1, keepdim=True)
+    correct = pred.eq(y.view_as(pred)).sum().item()
+    return correct / N
+
 # Check GPU
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 torch.cuda.is_available()
@@ -93,46 +131,9 @@ model = torch.compile(model)
 loss_function = nn.CrossEntropyLoss()
 optimizer = Adam(model.parameters())
 
-# Calculate Accuracy
+# Get Lens
 train_N = len(train_loader.dataset)
 valid_N = len(valid_loader.dataset)
-def get_batch_accuracy(output, y, N):
-    pred = output.argmax(dim=1, keepdim=True)
-    correct = pred.eq(y.view_as(pred)).sum().item()
-    return correct / N
-
-# Train Function
-def train():
-    loss = 0
-    accuracy = 0
-
-    model.train()
-    for x, y in train_loader:
-        x, y = x.to(device), y.to(device)
-        output = model(x)
-        optimizer.zero_grad()
-        batch_loss = loss_function(output, y)
-        batch_loss.backward()
-        optimizer.step()
-
-        loss += batch_loss.item()
-        accuracy += get_batch_accuracy(output, y, train_N)
-    print('Train - Loss: {:.4f} Accuracy: {:.4f}'.format(loss, accuracy))
-
-# Validation Function
-def validate():
-    loss = 0
-    accuracy = 0
-
-    model.eval()
-    with torch.no_grad():
-        for x, y in valid_loader:
-            x, y = x.to(device), y.to(device)
-            output = model(x)
-
-            loss += loss_function(output, y).item()
-            accuracy += get_batch_accuracy(output, y, valid_N)
-    print('Valid - Loss: {:.4f} Accuracy: {:.4f}'.format(loss, accuracy))
 
 # Training Loop
 epochs = 5
